1c3be39c2bd4c72a3b3320ffd843474676b23c45
Viktor Nagy <viktor.nagy1995@gmail.com>
2020-06-05 07:33:31 +0000

    template text changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,11 @@
         <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="/img/intro.jpg" alt="">
         <div class="intro-text left-0 text-center bg-faded p-5 rounded">
           <h2 class="section-heading mb-4">
-            <span class="section-heading-upper">Fresh Coffee</span>
-            <span class="section-heading-lower">Worth Drinking</span>
+            <span class="section-heading-lower">Vitajte na mojej stránke</span>
           </h2>
-          <p class="mb-3">Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!
-          </p>
+	  <p class="mb-3">Keramike a iným tvorivým činnostiam sa venujem už zopár rokov. Baví ma kreatívne tvorenie a uspokojuje ma dobrý pocit, ak sa mi niečo podarí. Pokiaľ sa vám nejaké moje výrobky a výtvory páčia, napíšte prosím a objednajte. Ak už daný originál nie je k dispozícii, nevadí, vyrobím "skoro taký istý" alebo podobný.</p>
           <div class="intro-button mx-auto">
-            <a class="btn btn-primary btn-xl" href="#">Visit Us Today!</a>
+            <a class="btn btn-primary btn-xl" href="/produkty">Ponuka</a>
           </div>
         </div>
       </div>
